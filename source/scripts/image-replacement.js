ca2b1d4ede01b045413cc91793b3a61eab8389e3
sohushah <shahsohil.9999@gmail.com>
2017-02-27 15:03:38 +0530

    changes on 26-2
    project from templates to projects

---
+++ source/scripts/image-replacement.js
@@ -0,0 +1,18 @@
+﻿function initImages() {
+    var imgDefer = document.getElementsByTagName('img');
+    for (var i = 0; i < imgDefer.length; i++) {
+        if (imgDefer[i].getAttribute('data-img')) {
+            imgDefer[i].setAttribute('src', imgDefer[i].getAttribute('data-img'));
+        }
+    }
+}
+window.onload = initImages;
+
+function init() {
+    var imgDefer = document.getElementsByTagName('img');
+    for (var i = 0; i < imgDefer.length; i++) {
+        if (imgDefer[i].getAttribute('data-img')) {
+            imgDefer[i].setAttribute('src', imgDefer[i].getAttribute('data-img'));
+        }
+    }
+}
